2824291ebfe0d0a0cef80b905d1905c427088dea
sudeepdeepu <sudep1997@gmail.com>
2022-04-02 19:18:14 +0530

    updaetd the code

---
+++ java/com/greatLearning/studentManagement/service/StudentServiceImpl.java
@@ -0,0 +1,68 @@
+package com.greatLearning.studentManagement.service;
+
+import java.util.List;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.greatLearning.studentManagement.entity.Student;
+
+
+@Repository
+public class StudentServiceImpl implements StudentService{
+	private SessionFactory sessionFactory;
+	private Session session;
+	
+	@Autowired
+	StudentServiceImpl(SessionFactory sessionFactory)
+	{
+		this.sessionFactory=sessionFactory;
+		try {
+			session=sessionFactory.getCurrentSession();
+		}
+		catch(HibernateException e){
+			session = sessionFactory.openSession();
+		}
+	}
+	
+	@Transactional
+	public List<Student> findAll(){
+		Transaction tx = session.beginTransaction();
+		List<Student> students = session.createQuery("from Student").list();
+		tx.commit();
+		return students;
+	}
+	
+	@Transactional
+	public Student findById(int id){
+		Student student = new Student();
+		Transaction tx = session.beginTransaction();
+		student = session.get(Student.class,id);
+		tx.commit();
+		return student;
+	}
+	
+	@Transactional
+	public void save(Student theStudent){
+		Transaction tx = session.beginTransaction();
+		session.saveOrUpdate(theStudent);
+		tx.commit();
+	}
+	
+	@Transactional
+	public void deleteById(int id){
+		Student student = new Student();
+		Transaction tx = session.beginTransaction();
+		student = session.get(Student.class,id);
+		session.delete(student);
+		tx.commit();
+	}
+	
+	
+}
